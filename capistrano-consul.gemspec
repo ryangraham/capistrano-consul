8611fece7ab3b1ec400f2ab8a300057ac1b89b63
Ryan Graham <ryangraham@gmail.com>
2015-06-23 16:08:54 -0700

    first working version

---
+++ capistrano-consul.gemspec
@@ -0,0 +1,16 @@
+require File.expand_path('../lib/capistrano/consul/version.rb', __FILE__)
+
+Gem::Specification.new do |s|
+  s.name          = 'capistrano-consul'
+  s.version       = Capistrano::Consul::VERSION
+  s.date          = "2015-06-23"
+  s.description   = "Consul discovery for Capistrano"
+  s.summary       = "Consul discovery for Capistrano"
+  s.authors       = ["Ryan Graham"]
+  s.email         = "ryan.graham@gmail.com"
+  s.homepage      = "http://github.com/ryangraham/capistrano-consul"
+  s.require_paths = ["lib"]
+  s.files         = `git ls-files`.split("\n") 
+
+  s.add_runtime_dependency "diplomat"
+end
---
+++ lib/capistrano/consul.rb
@@ -0,0 +1,33 @@
+require 'capistrano'
+require 'diplomat'
+
+unless Capistrano::Configuration.respond_to?(:instance)
+  abort 'capistrano-consul requires Capistrano >= 2'
+end
+
+module Capistrano
+  module Consul
+    def self.extend(configuration)
+      configuration.load do
+        Capistrano::Configuration.instance.load do
+
+          def service(service_name, *roles)
+            Diplomat.configure do |config|
+              config.url = fetch(:consul_url)
+              # TODO: pass through ssl opts
+            end
+
+            @nodes = Diplomat::Service.get(service_name, :all)
+            @nodes.each do |node|
+              server node.Address, *roles
+            end 
+          end 
+        end
+      end
+    end
+  end
+end
+
+if Capistrano::Configuration.instance
+  Capistrano::Consul.extend(Capistrano::Configuration.instance)
+end
