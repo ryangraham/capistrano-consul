db98462b2d0ec0cca8945952a95ed3fc15a50053
Ryan Graham <ryangraham@gmail.com>
2015-08-20 10:54:49 -0700

    keep ssl opts but dont ask for net-http-persistent

--- lib/capistrano/consul.rb
+++ lib/capistrano/consul.rb
@@ -14,7 +14,6 @@ module Capistrano
           def service(service_name, *roles)
             Diplomat.configure do |config|
               config.url = fetch(:consul_url, "http://localhost:8500")
-              config.adapter = :net_http_persistent
               config.options = fetch(:ssl_options, {})
             end
 
